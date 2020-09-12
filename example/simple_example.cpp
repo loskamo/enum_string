b7973969f184d6d54c1af844fa2dfbbde3202634
loskamo <oleksandr.yurchuk@binaryx.com>
2020-09-12 16:11:28 +0300

    basic enum_string implemented

---
+++ example/simple_example.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+#include "enum_string.h"
+
+DECLARE_ENUM(LogLevel,  // enum class LogLevel
+             Alert,     // LogLevel::Alert
+             Critical,  // LogLevel::Critical
+             Error,     // LogLevel::Error
+             Warning,   // LogLevel::Warning
+             Notice,    // LogLevel::Notice
+             Info,      // LogLevel::Info
+             Debug      // LogLevel::Debug
+             );
+
+int main() {
+  // serialize
+  std::cout << enum_to_string(LogLevel, Debug) << std::endl;
+
+  // deserialize
+  switch (string_to_enum(LogLevel, "INFO")) {
+    case LogLevel::Alert: {
+      std::cout << "ALERT" << std::endl;
+    } break;
+    case LogLevel::Critical: {
+      std::cout << "CRITICAL" << std::endl;
+    } break;
+    case LogLevel::Error: {
+      std::cout << "ERROR" << std::endl;
+    } break;
+    case LogLevel::Warning: {
+      std::cout << "WARN" << std::endl;
+    } break;
+    case LogLevel::Notice: {
+      std::cout << "NOTICE" << std::endl;
+    } break;
+    case LogLevel::Info: {
+      std::cout << "INFO" << std::endl;
+    } break;
+    case LogLevel::Debug: {
+      std::cout << "DEBUG" << std::endl;
+    } break;
+    case LogLevel::MAX: {
+      std::cout << "Incorrect value" << std::endl;
+    } break;
+  }
+
+  return 0;
+}
